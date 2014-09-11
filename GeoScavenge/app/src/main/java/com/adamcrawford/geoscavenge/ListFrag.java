4af912a98aed62e817d62401bcdf6dd554b33ec0
Adam Crawford <geeksbsmrt@gmail.com>
2014-09-10 22:27:04 -0400

    Start layout for menu, place it in the ListFrag class.

--- GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/ListFrag.java
+++ GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/ListFrag.java
@@ -3,8 +3,11 @@ package com.adamcrawford.geoscavenge;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 /**
  * Author:  Adam Crawford
@@ -20,6 +23,12 @@ public class ListFrag extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View listView = inflater.inflate(R.layout.fragment_main, container, false);
+        setHasOptionsMenu(true);
         return listView;
     }
-}
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }}
