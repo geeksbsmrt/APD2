a5dd8211737b41ce09579e43ecabc55515794e96
Adam Crawford <geeksbsmrt@gmail.com>
2014-09-11 20:29:49 -0400

    Fixed NPE due to huntList not being initialized.  Added log lines while troubleshooting.  Added basic placeholder data (will be replaced with Network data).

--- GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/ListFrag.java
+++ GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/ListFrag.java
@@ -2,6 +2,7 @@ package com.adamcrawford.geoscavenge;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,6 +12,10 @@ import android.widget.ListView;
 
 import com.adamcrawford.geoscavenge.hunt.HuntAdapter;
 import com.adamcrawford.geoscavenge.hunt.HuntConstructor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -24,6 +29,10 @@ import java.util.ArrayList;
  */
 public class ListFrag extends ListFragment {
 
+    String TAG = "LF";
+
+    JSONObject huntJSON = new JSONObject();
+
     private ArrayList<HuntConstructor> huntList;
 
     public ListFrag() {
@@ -33,9 +42,48 @@ public class ListFrag extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        HuntAdapter adapter = new HuntAdapter(getActivity(), R.layout.item_hunt, huntList);
+        huntList = new ArrayList<HuntConstructor>();
 
-        setListAdapter(adapter);
+        //This will be replaced by Network Data in future release
+        try {
+
+            Log.i(TAG, "building data");
+            JSONArray huntArray = new JSONArray();
+            JSONObject hunt1 = new JSONObject();
+            JSONObject hunt2 = new JSONObject();
+            hunt1.put("name","Hunt1");
+            hunt1.put("desc", "Hunt for buried treasure");
+            hunt2.put("name", "Hunt2");
+            hunt2.put("desc", "Find the love of your life");
+            huntArray.put(hunt1).put(hunt2);
+            huntJSON.put("hunts", huntArray);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        try {
+            Log.i(TAG, "Building list");
+            Log.i(TAG, huntJSON.toString());
+            JSONArray dataArray = huntJSON.getJSONArray("hunts");
+
+            for (int i = 0, j = dataArray.length(); i < j; i++) {
+                JSONObject hunt = (JSONObject) dataArray.get(i);
+                HuntConstructor hc = new HuntConstructor(hunt);
+                Log.i(TAG, hc.toString());
+                huntList.add(hc);
+                Log.i(TAG, hc.toString());
+            }
+
+            Log.i(TAG, huntList.toString());
+
+            Log.i(TAG, "building adapter");
+            HuntAdapter adapter = new HuntAdapter(getActivity(), R.layout.item_hunt, huntList);
+            adapter.notifyDataSetChanged();
+            Log.i(TAG,"Setting Adapter");
+            setListAdapter(adapter);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 
     @Override
--- GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/hunt/HuntConstructor.java
+++ GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/hunt/HuntConstructor.java
@@ -1,5 +1,12 @@
 package com.adamcrawford.geoscavenge.hunt;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
 /**
  * Author:  Adam Crawford
  * Project: GeoScavenge
@@ -7,9 +14,23 @@ package com.adamcrawford.geoscavenge.hunt;
  * File:    HuntConstructor
  * Purpose: TODO Minimum 2 sentence description
  */
-public class HuntConstructor {
+public class HuntConstructor implements Serializable {
+
+    String TAG = "HC";
 
     public String huntName;
     public String huntDesc;
+
+    public HuntConstructor (JSONObject object) {
+
+        Log.i(TAG, object.toString());
+
+        try {
+            this.huntName = object.getString("name");
+            this.huntDesc = object.getString("desc");
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
 
 }
