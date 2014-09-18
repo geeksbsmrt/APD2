9f52467b64682fafbda254c42ef08676c375df6d
Adam Crawford <geeksbsmrt@gmail.com>
2014-09-17 21:28:29 -0400

    optimized imports

--- GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/GuessActivity.java
+++ GeoScavenge/app/src/main/java/com/adamcrawford/geoscavenge/GuessActivity.java
@@ -7,17 +7,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.adamcrawford.geoscavenge.hunt.HuntConstructor;
 
 import org.json.JSONException;
 import org.json.JSONObject;
