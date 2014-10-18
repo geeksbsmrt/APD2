6890bcfc6e62e8a3305595fb72a9643ebf9d3763
Adam Crawford <geeksbsmrt@gmail.com>
2014-10-18 00:22:27 -0400

    Complete Game Item, pull game data based on selected venue, only show future games in the list

--- SoccerScheduler/app/src/main/java/com/adamcrawford/soccerscheduler/Fragment_VenueDetails.java
+++ SoccerScheduler/app/src/main/java/com/adamcrawford/soccerscheduler/Fragment_VenueDetails.java
@@ -9,6 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+import java.util.Date;
+
 /**
  * Author:  Adam Crawford
  * Project: Soccer Scheduler
@@ -19,6 +24,9 @@ import android.widget.TextView;
 public class Fragment_VenueDetails extends ListFragment {
 
     private String TAG = "FVD";
+    ParseQueryAdapter<GameItem> gameAdapter;
+    Bundle venueBundle;
+    VenueItem venue;
 
     public Fragment_VenueDetails(){}
 
@@ -26,17 +34,12 @@ public class Fragment_VenueDetails extends ListFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_venue_details, container, false);
 
-        Bundle venueBundle = getArguments();
-        VenueItem venue = (VenueItem) venueBundle.get("venue");
-
         MainActivity.actionBar.setTitle(venue.getVenueName());
 
-        TextView venueName = (TextView) rootView.findViewById(R.id.fvdName);
         TextView venueAddress = (TextView) rootView.findViewById(R.id.fvdAddress);
         TextView venueLoc = (TextView) rootView.findViewById(R.id.fvdLocation);
         TextView venueFields = (TextView) rootView.findViewById(R.id.numFields);
 
-        venueName.setText(venue.getVenueName());
         venueAddress.setText(venue.getVenueAddress());
         venueLoc.setText(venue.getVenueLocation());
         venueFields.setText(String.valueOf(venue.getVenueFields().length()));
@@ -44,5 +47,46 @@ public class Fragment_VenueDetails extends ListFragment {
         Log.i(TAG, venue.getVenueName());
 
         return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        venueBundle = getArguments();
+        venue = (VenueItem) venueBundle.get("venue");
+
+        ParseQueryAdapter.QueryFactory<GameItem> factory = new ParseQueryAdapter.QueryFactory<GameItem>() {
+            @Override
+            public ParseQuery<GameItem> create() {
+                ParseQuery<GameItem> query = GameItem.getQuery();
+                query.whereContains(GameItem.GAMEVENUE, venue.getObjectId());
+                query.whereGreaterThan(GameItem.GAMEDATE, new Date());
+                query.orderByAscending(GameItem.GAMEDATE);
+                return query;
+            }
+        };
+
+        gameAdapter = new ParseQueryAdapter<GameItem>(getActivity(), factory) {
+            @Override
+            public View getItemView(GameItem game, View view, ViewGroup parent){
+                if (view == null) {
+                    view = View.inflate(getContext(), R.layout.item_game, null);
+                }
+
+                TextView homeTeamView = (TextView) view.findViewById(R.id.homeTeam);
+                TextView awayTeamView = (TextView) view.findViewById(R.id.awayTeam);
+                TextView ageGroup = (TextView) view.findViewById(R.id.ageGroup);
+                TextView gameDate = (TextView) view.findViewById(R.id.gameDate);
+
+                homeTeamView.setText(game.getHomeTeam());
+                awayTeamView.setText(game.getAwayTeam());
+                ageGroup.setText(game.getAgeGroup());
+                gameDate.setText(game.getGameDate().toString());
+
+                return view;
+            }
+        };
+        setListAdapter(gameAdapter);
     }
 }
